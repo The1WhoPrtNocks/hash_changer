e9072de4224d19b73e9f09e3ff9fe0dc3ead962e
The1WhoPrtNocks <36970807+The1WhoPrtNocks@users.noreply.github.com>
2020-09-02 14:44:42 +0100

    Update changer.py

--- changer.py
+++ changer.py
@@ -6,7 +6,7 @@ with open(__file__,"r") as f:
         content.append(line)
 
 with open(__file__,"w") as f:
-    content[1] = "a = {n}\n".format(n=b)
-    content[2] = "b = {n}\n".format(n=a+b)
+    content[0] = "a = {n}\n".format(n=b)
+    content[1] = "b = {n}\n".format(n=a+b)
     for i in range(len(content)):
         f.write(content[i])
